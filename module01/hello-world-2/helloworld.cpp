2195560f324cb1dcf7006b502617fee54298ee7b
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-30 13:27:50 -0700

    renamed files

--- module01/hello-world-2/helloworld.cpp
+++ module01/hello-world-2/helloworld.cpp
@@ -1,11 +1,14 @@
 // helloworld.cpp
 // My first C++ Program - Dominic Torruellas
 #include <iostream>
+#include <string>
 
 constexpr auto greeting = "Hello, World!";
+auto another_greeting = [] (std::string m) { return std::string("hello ") + m; };
 
 int main() {
 
   std::cout << greeting << std::endl;
+  std::cout << another_greeting("Dom!") << std::endl;
 
 }
