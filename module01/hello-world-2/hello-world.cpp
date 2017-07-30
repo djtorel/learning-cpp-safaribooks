eb6b7f150af3d1af1deca1a0202c14773486e68a
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-30 13:23:14 -0700

    Hello World using some c++11 syntax

---
+++ module01/hello-world-2/hello-world.cpp
@@ -0,0 +1,11 @@
+// helloworld.cpp
+// My first C++ Program - Dominic Torruellas
+#include <iostream>
+
+constexpr auto greeting = "Hello, World!";
+
+int main() {
+
+  std::cout << greeting << std::endl;
+
+}
