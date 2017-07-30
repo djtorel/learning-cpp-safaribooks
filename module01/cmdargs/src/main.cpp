aa924ebb0a21faa9fdbb99380c63ddda52b5ea72
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-30 14:02:14 -0700

    restructured. also finished module 1 practicing command line arguments

---
+++ module01/cmdargs/src/main.cpp
@@ -0,0 +1,15 @@
+// main.cpp
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  if (argc < 6) {
+    std::cerr << "Invalid number of arugments - at least 5 required" << std::endl;
+    return 1;
+  }
+  std::cout << "Total arguments: " << argc << std::endl;
+
+  for (int i{0}; i < argc; i++) {
+    std::cout << "arg[" << i << "] = " << argv[i] << std::endl;
+  }
+}
